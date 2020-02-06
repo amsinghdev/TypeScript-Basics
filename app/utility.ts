62182c802c97b617c42f1ec6efb6ba85b256d45e
Amit Singh <amit8829.jnnce@gmail.com>
2020-02-06 22:40:26 +0530

    Initial Commit

---
+++ app/utility.ts
@@ -0,0 +1,10 @@
+
+     function getInputValue ( elementID: string ): string {
+
+        const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+        return inputElement.value;
+     }
+
+     export {getInputValue as getValue}
+
+
